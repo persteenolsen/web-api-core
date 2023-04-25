dff66225219bb876da570654464341ceb4871e5e
Per Steen Olsen <persteen22@hotmail.com>
2023-04-25 14:20:01 +0200

    Adjusting the http error handling - Model Validation - Post

--- WebApi/Entities/Post.cs
+++ WebApi/Entities/Post.cs
@@ -11,16 +11,17 @@ namespace WebApi.Entities
         
         public int Id { get; set; }
 
-        // NOTE: The validation fires if the Modelstate is not valid and a http status 400 will
+        // The validation fires if the Modelstate is not valid and a http status 400 will
         // be sent to the client - and should be handled by the client - Look the Angular Error handling for details
+        // Note: The ErrorMessages must not contain "," for the Angular error validation to handle them !!
         // Type varchar50 in the MS SQL DB
         [Required(ErrorMessage = "Title is required")]
-        [StringLength(10, MinimumLength = 2, ErrorMessage = "Title must be between 2 and 10 characters")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "Must be between 2 and 10 characters")]
         public string Title { get; set; }
 
-        // Note: Type varchar50 in the MS SQL DB
+        // Type varchar50 in the MS SQL DB
         [Required(ErrorMessage = "Body is required")]
-        [StringLength(15, MinimumLength = 2, ErrorMessage = "Body must be between 2 and 15 characters")]
+        [StringLength(15, MinimumLength = 2, ErrorMessage = "Must be between 2 and 15 characters")]
         public string Body { get; set; }
 
        // public string secret { get; set; }
